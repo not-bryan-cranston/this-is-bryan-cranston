dd8efc6790f13994824f77fb6133dff65edf0d27
Lindsey <letaylor622@gmail.com>
2016-11-12 20:26:48 -0500

    Add points
    Adding several points of interest

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // set default view here
-var map = L.map('map').setView([51.5, -0.09], 13);
+var map = L.map('map').setView([0, 0], 1);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -20,4 +20,13 @@ var LeafIcon = L.Icon.extend({
 var greenIcon = new LeafIcon({ iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png' });
 
 // add icons to map here
-L.marker([51.5, -0.09], { icon: greenIcon }).addTo(map);
+// L.marker([45.82087222, -71.26870833], { icon: greenIcon }).addTo(map);
+
+
+L.marker([45.82087222, -71.26870833]).addTo(map);
+L.marker([48.47663333, -78.91451667]).addTo(map);
+L.marker([48.26614722, -78.54778611]).addTo(map);
+L.marker([47.67407778, -81.79919444]).addTo(map);
+L.marker([14.37556389, 0.079280556]).addTo(map);
+L.marker([5.077191667, -55.15584167]).addTo(map);
+L.marker([48.257975, -78.50706111]).addTo(map);
